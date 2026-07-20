9634cab8c7df8efbedb34bd635ec2d0ff1b9549f
Aswanth KR <aswanthkr1234@gmail.com>
2026-07-20 15:20:30 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -415,6 +415,9 @@ def export_pdf():
     doc.build(elements)
 
     return send_file(filename, as_attachment=True)
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
 
